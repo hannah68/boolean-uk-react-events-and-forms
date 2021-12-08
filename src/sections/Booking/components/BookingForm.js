a135c0aec89d0e9c9b705cfb3517639cc8554256
hannah68 <reihaneh.naderi1989@gmail.com>
2021-12-08 12:49:49 +0000

    challenge done

--- src/sections/Booking/components/BookingForm.js
+++ src/sections/Booking/components/BookingForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import CheckboxesGroup from './CheckboxesGroup';
 
+// refactoring========================================
 const BookingForm = () => {
     const [productData, setProductData] = useState({
         date: "",
@@ -11,90 +13,191 @@ const BookingForm = () => {
         spaActivities: [],
     });
 
+    const checkboxes = [
+        {
+            id: 'pool',
+            name: 'pool',
+            label: 'Soak and Swim',
+        },
+        {
+            id: 'facial',
+            name: 'facial',
+            label: 'Facial',
+        },
+        {
+            id: 'massage',
+            name: 'massage',
+            label: 'Massage',
+        }
+    ];
+
+    const checkboxItems = [
+        {
+            id: 'newspaper',
+            name: 'newspaper',
+            label: 'Daily Newspaper',
+        },
+        {
+            id: 'breakfast',
+            name: 'breakfast',
+            label: 'Breakfast',
+        },
+    ];
+
+
     const handleChange = (e) => {
-        const name = e.target.name;
         const id = e.target.id;
         const checked = e.target.checked;
-        const value = e.target.value;
+        const name = e.target.name;
         const type = e.target.type;
+        const value = e.target.value;
 
         if(type === "datetime-local"){
             setProductData({...productData, date: value})
         }
-        if(id === "pool"){
+        if(checked){
             setProductData({...productData, spaActivities: [...productData.spaActivities, name]})
         }
-        if(id === "facial"){
-            setProductData({...productData, spaActivities: [...productData.spaActivities, name]})
+        if(checked && id === 'breakfast' || id === 'newspaper'){
+            setProductData({...productData, roomServices: {...productData.roomServices, [name] : checked}})
         }
-        if(id === "massage"){
-            setProductData({...productData, spaActivities: [...productData.spaActivities, name]})
-        }
-        if(id === "newspaper"){
-            setProductData({...productData, roomServices: {...productData.roomServices, [name]: checked}})
-        }
-        if(id === "breakfast"){
-            setProductData({...productData, roomServices: {...productData.roomServices, [name]: checked}})
-        }
-        console.log(productData);
     }
-   
+
     const submitFormHandler = (e) => {
         e.preventDefault();
         console.log({productData});
     }
 
-    
-
-
     return (
         <form className="form-stack" onSubmit={submitFormHandler}>
             <label htmlFor="date">When will you be arriving?</label>
-            <input type="datetime-local" onChange={handleChange}/>
+            <input type="datetime-local" onChange={ handleChange}/>
             <section>
                 <h3>Spa Activities</h3>
-                <input
-                    type="checkbox"
-                    id="pool"
-                    name="pool"
-                    onChange={handleChange}
-                />
-                <label htmlFor="pool">Soak and Swim</label>
-                <input
-                    type="checkbox"
-                    id="facial"
-                    name="facial"
-                    onChange={handleChange}
-                />
-                <label htmlFor="facial">Facial</label>
-                <input
-                    type="checkbox"
-                    id="massage"
-                    name="massage"
-                    onChange={handleChange}
-                />
-                <label htmlFor="massage">Massage</label>
+                {checkboxes.map((checkbox, index) => {
+                    return <CheckboxesGroup 
+                    checkbox={checkbox} 
+                    setProductData={setProductData} 
+                    productData={productData}
+                    handleChange={handleChange}
+                    key={index}
+                    />
+                })}
             </section>
             <section>
                 <h3>Room Services</h3>
-                <input 
-                    type="checkbox" 
-                    id="newspaper" 
-                    name="newspaper"
-                    onChange={handleChange}
-                />
-                <label htmlFor="newspaper">Daily Newspaper</label>
-                <input 
-                    type="checkbox" 
-                    id="breakfast" 
-                    name="breakfast"
-                    onChange={handleChange}
-                />
-                <label htmlFor="breakfast">Breakfast</label>
+                {checkboxItems.map((checkbox, index) => {
+                    return <CheckboxesGroup 
+                    checkbox={checkbox} 
+                    setProductData={setProductData} 
+                    productData={productData}
+                    handleChange={handleChange}
+                    key={index}
+                    />
+                })}
             </section>
             <button type="submit">Book</button>
         </form>
     )
 }
+
+
+// first way of doing===================================
+
+// const BookingForm = () => {
+//     const [productData, setProductData] = useState({
+//         date: "",
+//         roomServices: {
+//             newspaper: false,
+//             breakfast: false
+//         },
+//         spaActivities: [],
+//     });
+
+//     const handleChange = (e) => {
+//         const name = e.target.name;
+//         const id = e.target.id;
+//         const checked = e.target.checked;
+//         const value = e.target.value;
+//         const type = e.target.type;
+
+//         if(type === "datetime-local"){
+//             setProductData({...productData, date: value})
+//         }
+//         if(id === "pool"){
+//             setProductData({...productData, spaActivities: [...productData.spaActivities, name]})
+//         }
+//         if(id === "facial"){
+//             setProductData({...productData, spaActivities: [...productData.spaActivities, name]})
+//         }
+//         if(id === "massage"){
+//             setProductData({...productData, spaActivities: [...productData.spaActivities, name]})
+//         }
+//         if(id === "newspaper"){
+//             setProductData({...productData, roomServices: {...productData.roomServices, [name]: checked}})
+//         }
+//         if(id === "breakfast"){
+//             setProductData({...productData, roomServices: {...productData.roomServices, [name]: checked}})
+//         }
+//         console.log(productData);
+//     }
+   
+//     const submitFormHandler = (e) => {
+//         e.preventDefault();
+//         console.log({productData});
+//     }
+
+    
+
+
+//     return (
+//         <form className="form-stack" onSubmit={submitFormHandler}>
+//             <label htmlFor="date">When will you be arriving?</label>
+//             <input type="datetime-local" onChange={handleChange}/>
+//             <section>
+//                 <h3>Spa Activities</h3>
+//                 <input
+//                     type="checkbox"
+//                     id="pool"
+//                     name="pool"
+//                     onChange={handleChange}
+//                 />
+//                 <label htmlFor="pool">Soak and Swim</label>
+//                 <input
+//                     type="checkbox"
+//                     id="facial"
+//                     name="facial"
+//                     onChange={handleChange}
+//                 />
+//                 <label htmlFor="facial">Facial</label>
+//                 <input
+//                     type="checkbox"
+//                     id="massage"
+//                     name="massage"
+//                     onChange={handleChange}
+//                 />
+//                 <label htmlFor="massage">Massage</label>
+//             </section>
+//             <section>
+//                 <h3>Room Services</h3>
+//                 <input 
+//                     type="checkbox" 
+//                     id="newspaper" 
+//                     name="newspaper"
+//                     onChange={handleChange}
+//                 />
+//                 <label htmlFor="newspaper">Daily Newspaper</label>
+//                 <input 
+//                     type="checkbox" 
+//                     id="breakfast" 
+//                     name="breakfast"
+//                     onChange={handleChange}
+//                 />
+//                 <label htmlFor="breakfast">Breakfast</label>
+//             </section>
+//             <button type="submit">Book</button>
+//         </form>
+//     )
+// }
 
 export default BookingForm
--- src/sections/Booking/components/CheckboxesGroup.js
+++ src/sections/Booking/components/CheckboxesGroup.js
@@ -1,1 +1,20 @@
+import React from 'react';
+
+const CheckboxesGroup = (props) => {
+    const {id, name, label} = props.checkbox;
+    
+    return (
+        <div>
+            <input 
+                type="checkbox" 
+                id={id} 
+                name={name}
+                onChange={props.handleChange}
+            />
+            <label htmlFor={id}>{label}</label>
+        </div>
+    )
+}
+
+export default CheckboxesGroup
 
